9df978f178d47821bd1995b250e86a78debfa452
gdi2290 <github@gdi2290.com>
2014-06-01 21:06:46 -0700

    add cleanup to camera plugin

--- src/plugins/camera.js
+++ src/plugins/camera.js
@@ -6,15 +6,26 @@ angular.module('ngCordova.plugins.camera', [])
     getPicture: function(options) {
       var q = $q.defer();
 
-      navigator.camera.getPicture(function(result) {
+      navigator.camera.getPicture(function(imageData) {
         // Do any magic you need
-        q.resolve(result);
+        q.resolve(imageData);
       }, function(err) {
         q.reject(err);
       }, options);
 
       return q.promise;
+    },
+    cleanup: function(options) {
+      var q = $q.defer();
+      navigator.camera.cleanup(function() {
+        // Do any magic you need
+        q.resolve(arguments);
+      }, function(err) {
+        q.reject(err);
+      });
+
+      return q.promise;
     }
+    
   }
 }]);
-.
