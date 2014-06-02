b1ac9a309ed2a700af1bad52dcac4626ede438be
Max Lynch <max@drifty.com>
2014-06-02 18:24:33 -0500

    Updated

--- demo/app/init.sh
+++ demo/app/init.sh
@@ -4,16 +4,16 @@ function ap {
   cordova plugin add org.apache.cordova.$1
 }
 
-#ap device
-#ap camera
-#ap vibration
-#ap network-information
-#ap file
-#ap geolocation
+ap device
+ap camera
+ap vibration
+ap network-information
+ap file
+ap geolocation
 ap dialogs
 ap device-motion
 ap splashscreen
 ap device-orientation
 ap contacts
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard
-
+cordova plugin add https://github.com/phonegap-build/BarcodeScanner.git
