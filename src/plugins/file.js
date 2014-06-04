c478aa6d2d76839670c5509cbf6fb2d3c85ff6e0
Yacine Rezgui <rezgui.y@gmail.com>
2014-06-04 14:08:12 +0100

    Fix bad save of file

--- src/plugins/file.js
+++ src/plugins/file.js
@@ -1,72 +1,23 @@
 angular.module('ngCordova.plugins.file', [])
 
-.factory('File', ['$q', function ($q) {
+.factory('File', ['$window', '$q', function ($window, $q) {
 
   return {
-    save: function (contact) {
+    requestFileSystem: function (type, size) {
       var q = $q.defer();
-      var deviceContact = navigator.contacts.create(contact);
+      
+      $window.requestFileSystem(type, size,
 
-      deviceContact.save(function (result) {
+        function (result) {
           q.resolve(result);
         },
         function (err) {
           q.reject(err);
-        });
-      return q.promise;
-    },
-
-    remove: function (contact) {
-      var q = $q.defer();
-      var deviceContact = navigator.contacts.create(contact);
-
-      deviceContact.remove(function (result) {
-          q.resolve(result);
-        },
-        function (err) {
-          q.reject(err);
-        });
-      return q.promise;
-    },
-
-    clone: function (contact) {
-      var deviceContact = navigator.contacts.create(contact);
-      return deviceContact.clone(contact)
-    },
-
-    find: function (options) {
-      var q = $q.defer();
-      var fields = options.fields || ['id', 'displayName'];
-      delete options.fields;
-
-      navigator.contacts.find(fields, function (results) {
-          q.resolve(results);
-        },
-        function (err) {
-          q.reject(err);
-        },
-        options);
+        }
+      );
 
       return q.promise;
     }
-
-    /*
-     getContact: function (contact) {
-     var q = $q.defer();
-
-     navigator.contacts.pickContact(function (contact) {
-
-     })
-
-     }
-     */
-
-    // TODO: method to set / get ContactAddress
-    // TODO: method to set / get ContactError
-    // TODO: method to set / get ContactField
-    // TODO: method to set / get ContactName
-    // TODO: method to set / get ContactOrganization
-
-  }
+  };
 
 }]);
