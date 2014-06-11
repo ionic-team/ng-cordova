723d4fa29cae53af09e6c5fffe9f3a88a2124023
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-06-11 10:36:04 +0200

    Add Toast Plugin

---
+++ src/plugins/toast.js
@@ -0,0 +1,79 @@
+angular.module('ngCordova.plugins.toast', [])
+
+.factory('$cordovaToast', ['$q', function ($q) {
+
+    return {
+      showShortTop: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showShortTop(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+      showShortCenter: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showShortCenter(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+      showShortBottom: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showShortBottom(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+      showLongTop: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showLongTop(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+      showLongCenter: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showLongCenter(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+      showLongBottom: function (message) {
+        var q = $q.defer();
+        window.plugins.toast.showLongBottom(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      },
+
+
+      show: function (message, duration, position) {
+        var q = $q.defer();
+        window.plugins.toast.show(message, duration, position, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error)
+        })
+        return q.promise;
+      }
+    }
+
+  }
+]);
