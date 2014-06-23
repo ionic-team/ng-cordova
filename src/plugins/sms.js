7c1ff6bbe30544073cdfe51063ecee65cb7310ec
fraserxu <xvfeng123@gmail.com>
2014-06-23 17:53:28 +0800

    add sms plugin

---
+++ src/plugins/sms.js
@@ -0,0 +1,17 @@
+angular.module('ngCordova.plugins.sms', [])
+
+.factory('$cordovaSms', [function () {
+
+    return {
+      send: function (number, message, intent, cb) {
+        sms.send(number, message, intent, function (res) {
+          cb(null, res);
+        }, function (err) {
+          if(err) {
+            cb(err);
+          }
+        });
+      }
+    }
+  }
+]);
