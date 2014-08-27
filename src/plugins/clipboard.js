f54bdc8efc893373b2f421c48022422ac2c28735
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-08-27 11:58:18 +0200

    newPlugin(clipboard): new plugin with promise fix

---
+++ src/plugins/clipboard.js
@@ -0,0 +1,31 @@
+angular.module('ngCordova.plugins.clipboard', [])
+
+  .factory('$cordovaClipboard', ['$q', function ($q) {
+
+    return {
+      copy: function (text) {
+        var q = $q.defer();
+
+        window.cordova.plugins.clipboard.copy(text,
+          function () {
+            q.resolve();
+          }, function () {
+            q.reject();
+          });
+
+        return q.promise;
+      },
+
+      paste: function () {
+        var q = $q.defer();
+
+        window.cordova.plugins.clipboard.copy(function () {
+          q.resolve();
+        }, function () {
+          q.reject();
+        });
+
+        return q.promise;
+      }
+    }
+  }]);
--- src/plugins/module.js
+++ src/plugins/module.js
@@ -32,5 +32,6 @@ angular.module('ngCordova.plugins', [
   'ngCordova.plugins.backgroundGeolocation',
   'ngCordova.plugins.facebookConnect',
   'ngCordova.plugins.adMob',
-  'ngCordova.plugins.googleMap'
+  'ngCordova.plugins.googleMap',
+  'ngCordova.plugins.clipboard'
 ]);
