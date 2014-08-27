767290705776f21262c9a9c00b2324ea14bbc406
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-08-27 18:30:00 +0200

    fix(demo): remove xcode project

--- ngdemo/platforms/ios/HelloCordova/Plugins/nl.x-services.plugins.toast/Toast.h
+++
@@ -1,7 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface Toast : CDVPlugin
-
-- (void)show:(CDVInvokedUrlCommand*)command;
-
-@end
--- ngdemo/www/app/barcodeScanner/barcodeScanner.ctrl.js
+++ ngdemo/www/app/barcodeScanner/barcodeScanner.ctrl.js
@@ -3,11 +3,13 @@ angular.module('demo.barcodeScanner.ctrl', [])
   .controller('BarcodeScannerCtrl', function ($scope, $cordovaBarcodeScanner) {
 
     $scope.scan = function () {
-      $cordovaBarcodeScanner.scan().then(function (result) {
-        $scope.scanResult = JSON.stringify(result);
-      }, function (err) {
-        $scope.scanResult = 'SCAN ERROR (see console)';
-        console.error(err);
-      });
+      $cordovaBarcodeScanner
+        .scan()
+        .then(function (result) {
+          $scope.scanResult = result;
+        }, function (err) {
+          $scope.scanResult = 'SCAN ERROR (see console)';
+          console.error(err);
+        });
     }
   });
