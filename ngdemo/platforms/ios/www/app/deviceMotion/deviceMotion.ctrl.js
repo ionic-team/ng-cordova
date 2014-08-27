f023dd28cef9db5d1672b7968227ccd29833e623
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-08-27 18:08:30 +0200

    chore(deviceMotion): fix

--- ngdemo/platforms/ios/www/app/deviceMotion/deviceMotion.ctrl.js
+++ ngdemo/platforms/ios/www/app/deviceMotion/deviceMotion.ctrl.js
@@ -3,47 +3,42 @@ angular.module('demo.deviceMotion.ctrl', [])
   .controller('DeviceMotionCtrl', function ($scope, $cordovaDeviceMotion) {
     var watchID;
 
-    var updateMotion = function (result) {
-      $scope.x = result.x;
-      $scope.y = result.y;
-      $scope.z = result.z;
-      $scope.timeStammp = result.timeStamp;
-      $scope.msg = "";
-      console.log("updated device motion");
-    };
-
     $scope.getAcceleration = function () {
-      $cordovaDeviceMotion.getCurrentAcceleration().then(function (result) {
-        updateMotion(result);
-      }, function (err) {
-        $scope.msg = err.message;
-      });
+      $cordovaDeviceMotion
+        .getCurrentAcceleration()
+        .then(function (motion) {
+          $scope.motion = motion;
+        }, function (err) {
+          $scope.msg = err.message;
+        });
     };
 
     $scope.watchAcceleration = function () {
       var options = { frequency: 3000 };  // Update every 3 seconds
 
-      watchID = $cordovaDeviceMotion.watchAcceleration(options).then(
-        function () {
-          /* unused */
+      $scope.this_watch = $cordovaDeviceMotion.watchAcceleration(options);
+
+      $scope.this_watch.promise.then(
+        function () {  /* unused */
         },
         function (err) {
-
           $scope.msg = err.message;
         },
-        function (acceleration) {
-          updateMotion(acceleration);
+        function (motion) {
+          $scope.motion = motion;
         });
     };
 
     $scope.clearWatch = function () {
       // use watchID from watchAccelaration()
 
-      $cordovaDeviceMotion.clearWatch(watchID).then(function (result) {
-        $scope.msg = "watch cleared";
-      }, function (err) {
-        $scope.msg = err.message;
-      });
+      $cordovaDeviceMotion
+        .clearWatch($scope.this_watch.watchID)
+        .then(function (result) {
+          $scope.msg = "watch cleared";
+        }, function (err) {
+          $scope.msg = err.message;
+        });
     }
   });
 
