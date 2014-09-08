c1b1bac2f2208fa030ce8e0c33343ccbb7ca3b71
Marc Zbyszynski <mmmzzz@gmail.com>
2014-09-08 16:31:59 -0400

    Forgot you can't inject dependencies into a provider

--- src/plugins/file.js
+++ src/plugins/file.js
@@ -10,9 +10,9 @@
 angular.module('ngCordova.plugins.file', [])
 
 //Filesystem (checkDir, createDir, checkFile, creatFile, removeFile, writeFile, readFile)
-.provider('$cordovaFile', ['$window', function ($window) {
+.provider('$cordovaFile', [function () {
 
-  var _fileSystemType = $window.LocalFileSystem ? LocalFileSystem.PERSISTENT : 0;
+  var _fileSystemType = window.LocalFileSystem ? window.LocalFileSystem.PERSISTENT : 0;
   var _fileSystemSize = 1024 * 1024;
 
   var fsProvider = {
