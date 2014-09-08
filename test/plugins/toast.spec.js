f5424f9280eda64494f36326a331a511130828b6
mrzmyr <m0ritz.m3yer@gmail.com>
2014-09-08 22:06:00 +0200

    test($cordovaToast): add unit tests

---
+++ test/plugins/toast.spec.js
@@ -0,0 +1,84 @@
+describe('Service: $cordovaToast', function() {
+
+  var $cordovaToast, $rootScope;
+
+  var functionNames = [
+    'showShortTop',
+    'showShortCenter',
+    'showShortBottom',
+    'showLongTop',
+    'showLongCenter',
+    'showLongBottom'
+  ];
+
+  beforeEach(module('ngCordova.plugins.toast'));
+
+  beforeEach(inject(function (_$cordovaToast_, _$q_, _$rootScope_) {
+    $cordovaToast = _$cordovaToast_;
+    $rootScope = _$rootScope_;
+
+    window.plugins = {
+      toast: {
+        show: angular.noop
+      }
+    };
+
+    for (var i = 0; i < functionNames.length; i++) {
+      window.plugins.toast[functionNames[i]] = angular.noop;
+    }
+  }));
+
+  for (var i = 0; i < functionNames.length; i++) {
+
+    (function (fnName) {
+
+      it('should call window\'s plugins.toast.' + fnName + ' method', function() {
+
+        var result;
+
+        spyOn(window.plugins.toast, fnName)
+          .andCallFake(function (message, successCb, errorCb) {
+            successCb(true);
+          });
+
+        $cordovaToast[fnName]('some message')
+          .then(function (response) {
+            result = response;
+          });
+
+        $rootScope.$digest();
+
+        expect(result).toBe(true);
+        expect(window.plugins.toast[fnName].calls[0].args[0]).toBe('some message');
+      });
+
+    })(functionNames[i]);
+  }
+
+  it('should call window\'s plugins.toast.show method', function() {
+
+    var result;
+
+    spyOn(window.plugins.toast, 'show')
+      .andCallFake(function (message, duration, position, successCb, errorCb) {
+        successCb(true);
+      });
+
+    $cordovaToast.show('some message', 1000, 'top')
+      .then(function (response) {
+        result = response;
+      });
+
+    $rootScope.$digest();
+
+    expect(result).toBe(true);
+    expect(window.plugins.toast.show).toHaveBeenCalledWith(
+      'some message',
+      1000,
+      'top',
+      jasmine.any(Function),
+      jasmine.any(Function)
+    );
+  });
+
+});
