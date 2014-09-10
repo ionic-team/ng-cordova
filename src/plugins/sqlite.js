107eacb7a27f6ac05304f40d47ac8c250061b931
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-09-10 15:03:03 -0400

    fix(sqlite): refractor openDB() + added deleteDB()

--- src/plugins/sqlite.js
+++ src/plugins/sqlite.js
@@ -6,8 +6,9 @@ angular.module('ngCordova.plugins.sqlite', [])
   .factory('$cordovaSQLite', ['$q', function ($q) {
 
     return  {
-      openDB: function (dbName) {
-        return  window.sqlitePlugin.openDatabase({name: dbName});
+      openDB: function (dbName, background) {
+        background = (typeof background === "undefined") ? 0 : background;
+        return  window.sqlitePlugin.openDatabase({name: dbName, bgType: background});
       },
 
       execute: function (db, query, binding) {
@@ -39,8 +40,18 @@ angular.module('ngCordova.plugins.sqlite', [])
           });
 
         return q.promise;
-      }
+      },
 
-      // more methods here
+      deleteDB: function (dbName) {
+        var q = $q.defer();
+
+        window.sqlitePlugin.deleteDatabase(dbName, function (success) {
+          q.resolve(success);
+        }, function (error) {
+          q.reject(error)
+        });
+
+        return q.promise;
+      }
     }
   }]);
