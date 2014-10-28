9aead733ba66a158106ded4a82c6c920ccd942d8
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-10-27 23:21:47 -0400

    new(plugin:appRating): new app rating plugin

--- src/plugins/ble.js
+++ src/plugins/ble.js
@@ -1,5 +1,5 @@
-//  install   :   cordova plugin add org.apache.cordova.battery-status
-//  link      :   https://github.com/apache/cordova-plugin-battery-status/blob/master/doc/index.md
+//  install   :   cordova plugin add https://github.com/don/cordova-plugin-ble-central#:/plugin
+//  link      :   https://github.com/don/cordova-plugin-ble-central
 
 angular.module('ngCordova.plugins.ble', [])
 
