0ad87b6a7fd076871701744967696525f7968579
Chua Kiem Fai <kievechua@gmail.com>
2014-10-29 09:59:03 +0800

    Allow spinner dialog to set fixed dialog

--- src/plugins/spinnerDialog.js
+++ src/plugins/spinnerDialog.js
@@ -6,8 +6,8 @@ angular.module('ngCordova.plugins.spinnerDialog', [])
   .factory('$cordovaSpinnerDialog', ['$window', function ($window) {
 
     return {
-      show: function (title, message) {
-        return $window.plugins.spinnerDialog.show(title, message);
+      show: function (title, message, fixed) {
+        return $window.plugins.spinnerDialog.show(title, message, fixed);
       },
       hide: function () {
         return $window.plugins.spinnerDialog.hide();
