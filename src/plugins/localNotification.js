961a1718f6ed9b49420cb78bf6662e4108dccd0f
Rémi Becheras <floss@remi-becheras.com>
2014-12-17 09:32:30 +0100

    Fix the $cordovaLocalNotification events handling that wasn't work at all;
    This PR fix the issue https://github.com/driftyco/ng-cordova/issues/310.
    
    The folowwing methods are wrapper that must handle the events for the local notifcations :
    
        $cordovaLocalNotification.onadd()
    
        $cordovaLocalNotification.ontrigger()
    
        $cordovaLocalNotification.onclick()
    
        $cordovaLocalNotification.oncancel()
    
    The previous code just returned the reference to the event property of the plugin :
    
        onclick: function () {
            return $window.plugin.notification.local.onclick;
        },
    
    Causing no way to use the method to assign a callback function.
    
    So I refactored the four methods to get a function `fn` in argument and assign it to the plugin event property.
    
    Plus, I rewriten the design of the module defining on top an object `cordovaLocalNotification`, attaching method's service to it  and returning it at the end. 
    
    Thus, each "event handler" can assign its callback then returning the factory instance and so  we can use and chain these methods like this :
    
      $cordovaLocalNotification
        .onadd(_onAdd)
        .ontrigger(_onTrigger)
        .onclick(_onClick)
        .oncancel(_onCancel);
    
      function _onAdd(id, state, json){
        // ...
      }
      function _onTrigger(id, state, json){
        // ...
      }
      function _onClick(id, state, json){
        // ...
      }
      function _onCancel(id, state, json){
        // ...
      }

--- src/plugins/localNotification.js
+++ src/plugins/localNotification.js
@@ -5,121 +5,129 @@ angular.module('ngCordova.plugins.localNotification', [])
 
   .factory('$cordovaLocalNotification', ['$q', '$window', function ($q, $window) {
 
-    return {
-      add: function (options, scope) {
-        var q = $q.defer();
-        $window.plugin.notification.local.add(
-          options,
-          function (result) {
-            q.resolve(result);
-          },
-          scope);
-        return q.promise;
-      },
+    var cordovaLocalNotification = {};
 
-      cancel: function (id, scope) {
-        var q = $q.defer();
-        $window.plugin.notification.local.cancel(
-          id, function (result) {
-            q.resolve(result);
-          }, scope);
 
-        return q.promise;
-      },
-
-      cancelAll: function (scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.cancelAll(
-          function (result) {
-            q.resolve(result);
-          }, scope);
-
-        return q.promise;
-      },
-
-      isScheduled: function (id, scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.isScheduled(
-          id,
-          function (result) {
-            q.resolve(result);
-          }, scope);
-
-        return q.promise;
-      },
-
-      hasPermission: function (scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.hasPermission(
-          function (badge) {
-            q.resolve(badge);
-          }, scope);
-
-        return q.promise;
-      },
-
-      promptForPermission: function () {
-        $window.plugin.notification.local.promptForPermission();
-      },
-
-      getScheduledIds: function (scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.getScheduledIds(
-          function (result) {
-            q.resolve(result);
-          }, scope);
-
-        return q.promise;
-      },
-
-      isTriggered: function (id, scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.isTriggered(
-          id, function (result) {
-            q.resolve(result);
-          }, scope);
-
-        return q.promise;
-      },
-
-      getTriggeredIds: function (scope) {
-        var q = $q.defer();
-
-        $window.plugin.notification.local.getTriggeredIds(
-          function (result) {
-            q.resolve(result);
-          }, scope);
-
-        return q.promise;
-      },
-
-      getDefaults: function () {
-        return $window.plugin.notification.local.getDefaults();
-      },
-
-      setDefaults: function (Object) {
-        $window.plugin.notification.local.setDefaults(Object);
-      },
-
-      onadd: function () {
-        return $window.plugin.notification.local.onadd;
-      },
-
-      ontrigger: function () {
-        return $window.plugin.notification.local.ontrigger;
-      },
-
-      onclick: function () {
-        return $window.plugin.notification.local.onclick;
-      },
-
-      oncancel: function () {
-        return $window.plugin.notification.local.oncancel;
-      }
+    cordovaLocalNotification.add = function (options, scope) {
+      var q = $q.defer();
+      $window.plugin.notification.local.add(
+        options,
+        function (result) {
+          q.resolve(result);
+        },
+        scope);
+      return q.promise;
     };
+
+    cordovaLocalNotification.cancel: function (id, scope) {
+      var q = $q.defer();
+      $window.plugin.notification.local.cancel(
+        id, function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.cancelAll: function (scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.cancelAll(
+        function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.isScheduled: function (id, scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.isScheduled(
+        id,
+        function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.hasPermission: function (scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.hasPermission(
+        function (badge) {
+          q.resolve(badge);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.promptForPermission: function () {
+      $window.plugin.notification.local.promptForPermission();
+    };
+
+    cordovaLocalNotification.getScheduledIds: function (scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.getScheduledIds(
+        function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.isTriggered: function (id, scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.isTriggered(
+        id, function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.getTriggeredIds: function (scope) {
+      var q = $q.defer();
+
+      $window.plugin.notification.local.getTriggeredIds(
+        function (result) {
+          q.resolve(result);
+        }, scope);
+
+      return q.promise;
+    };
+
+    cordovaLocalNotification.getDefaults: function () {
+      return $window.plugin.notification.local.getDefaults();
+    };
+
+    cordovaLocalNotification.setDefaults: function (Object) {
+      $window.plugin.notification.local.setDefaults(Object);
+    };
+
+    cordovaLocalNotification.onadd: function (fn) {
+      $window.plugin.notification.local.onadd = fn;
+      return cordovaLocalNotification;
+    };
+
+    cordovaLocalNotification.ontrigger: function (fn) {
+      $window.plugin.notification.local.ontrigger = fn;
+      return cordovaLocalNotification;
+    };
+
+    cordovaLocalNotification.onclick: function (fn) {
+      $window.plugin.notification.local.onclick = fn;
+      return cordovaLocalNotification;
+    };
+
+    cordovaLocalNotification.oncancel: function (fn) {
+      $window.plugin.notification.local.oncancel = fn;
+      return cordovaLocalNotification;
+    };
+
+    return cordovaLocalNotification;
+  
   }]);
