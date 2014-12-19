da2a1d80d214a0c87fc42d985eacc78f491edd61
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-12-19 13:51:38 +0100

    new(plugin:actionSheet):

--- demo/www/lib/ngCordova/ng-cordova.js
+++ demo/www/lib/ngCordova/ng-cordova.js
@@ -10,6 +10,30 @@ angular.module('ngCordova', [
   'ngCordova.plugins'
 ]);
 
+// install  :     cordova plugin add https://github.com/EddyVerbruggen/cordova-plugin-actionsheet.git
+// link     :     https://github.com/EddyVerbruggen/cordova-plugin-actionsheet
+
+angular.module('ngCordova.plugins.actionSheet', [])
+
+  .factory('$cordovaActionSheet', ['$q', '$window', function ($q, $window) {
+
+    return {
+      show: function (options) {
+        var q = $q.defer();
+
+        $window.plugins.actionsheet.show(options, function (result) {
+          q.resolve(result);
+        });
+
+        return q.promise;
+      },
+
+      hide: function () {
+        return $window.plugins.actionsheet.hide();
+      }
+    };
+  }]);
+
 // install  :     cordova plugin add https://github.com/floatinghotpot/cordova-plugin-admob.git
 // link     :     https://github.com/floatinghotpot/cordova-plugin-admob
 
@@ -3418,6 +3442,7 @@ angular.module('ngCordova.plugins.mobfoxAds', [])
   }]);
 
 angular.module('ngCordova.plugins', [
+  'ngCordova.plugins.actionSheet',
   'ngCordova.plugins.adMob',
   'ngCordova.plugins.appAvailability',
   'ngCordova.plugins.appRate',
