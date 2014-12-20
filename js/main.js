8e3964ca904c5c31ca0cda7948a3fc9fc060e080
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-12-20 23:37:37 +0100

    fix(docs): fix active links

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
 (function () {
-    smoothScroll.init({
-        speed: 1000,
-        easing: 'easeInOutQuart',
-        updateURL: true,
-        offset: 0,
-        callbackBefore: function (toggle, anchor) {
-            $('[data-scroll]').removeClass('active');
-            $(toggle).addClass('active');
-        },
-        callbackAfter: function (toggle, anchor) {
-
-        }
-    });
-
-    var options = {
-        valueNames: [ 'plugin']
-    };
-
-    var userList = new List('plugins', options);
-
-
     // left menu link highlight
     var leftMenu = $('.left-menu');
     var activeLink = leftMenu.find('[href="' + window.location.pathname + '"]');
