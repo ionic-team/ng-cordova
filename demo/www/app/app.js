e966e5475d33546a5b82c94c03f232c699cada3f
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-12-22 00:41:44 +0100

    fix(demo): cleanup

--- demo/www/app/app.js
+++ demo/www/app/app.js
@@ -47,7 +47,7 @@ angular.module('demo', [
     if (!window.cordova) {
       var appID = 1234567890;
       var version = "v2.0"; // or leave blank and default is v2.0
-      $cordovaFacebookProvider.setAppID(appID, version);
+      $cordovaFacebookProvider.browserInit(appID, version);
     }
 
     $stateProvider
