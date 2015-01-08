e0b24573c3c5c0884a7a981e91ecc616a4653075
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2015-01-08 08:36:29 -0500

    fix(plugin:0Auth): fix promise resolve #544

--- dist/ng-cordova.js
+++ dist/ng-cordova.js
@@ -3925,8 +3925,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -3964,12 +3964,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                   })
                   .error(function (data, status) {
                     deferred.reject("Problem authenticating");
-                  });
-                browserRef.close();
+                  })
+                  .finally(function () {
+                    browserRef.close();
+                  })
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4014,8 +4016,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4051,12 +4053,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                   })
                   .error(function (data, status) {
                     deferred.reject("Problem authenticating");
+                  }).
+                  finally(function () {
+                    browserRef.close();
                   });
-                browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4100,8 +4104,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4141,12 +4145,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                   })
                   .error(function (data, status) {
                     deferred.reject("Problem authenticating");
+                  })
+                  .finally(function () {
+                    browserRef.close();
                   });
-                browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4189,8 +4195,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4229,12 +4235,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                   })
                   .error(function (data, status) {
                     deferred.reject("Problem authenticating");
+                  })
+                  .finally(function () {
+                    browserRef.close();
                   });
-                browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4270,12 +4278,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                   })
                   .error(function (data, status) {
                     deferred.reject("Problem authenticating");
+                  })
+                  .finally(function () {
+                    browserRef.close();
                   });
-                browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4351,12 +4361,14 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                         })
                         .error(function (error) {
                           deferred.reject(error);
+                        })
+                        .finally(function () {
+                          browserRef.close();
                         });
-                      browserRef.close();
                     }
                   });
-                  browserRef.addEventListener('exit', function(event) {
-                      deferred.reject("The sign in flow was canceled");
+                  browserRef.addEventListener('exit', function (event) {
+                    deferred.reject("The sign in flow was canceled");
                   });
                 })
                 .error(function (error) {
@@ -4402,8 +4414,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4446,8 +4458,8 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
                 browserRef.close();
               }
             });
-            browserRef.addEventListener('exit', function(event) {
-                deferred.reject("The sign in flow was canceled");
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
             });
           } else {
             deferred.reject("Could not find InAppBrowser plugin");
@@ -4456,107 +4468,109 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
           deferred.reject("Cannot authenticate via a web browser");
         }
         return deferred.promise;
-    },
+      },
 
-    /*
-    * Sign into the Salesforce service
-    *
-    * Suggestion: use salesforce oauth with forcetk.js(as SDK)
-    *
-    * @param    string loginUrl (such as: https://login.salesforce.com ; please notice community login)
-    * @param    string clientId (copy from connection app info)
-    * @param    string redirectUri (callback url in connection app info)
-    * @return   promise
-    */
-    salesforce: function (loginUrl, clientId) {
+      /*
+       * Sign into the Salesforce service
+       *
+       * Suggestion: use salesforce oauth with forcetk.js(as SDK)
+       *
+       * @param    string loginUrl (such as: https://login.salesforce.com ; please notice community login)
+       * @param    string clientId (copy from connection app info)
+       * @param    string redirectUri (callback url in connection app info)
+       * @return   promise
+       */
+      salesforce: function (loginUrl, clientId) {
         var redirectUri = 'http://localhost/callback';
         var getAuthorizeUrl = function (loginUrl, clientId, redirectUri) {
-            return loginUrl+'services/oauth2/authorize?display=touch'+
-            '&response_type=token&client_id='+escape(clientId)+
-            '&redirect_uri='+escape(redirectUri);
+          return loginUrl + 'services/oauth2/authorize?display=touch' +
+            '&response_type=token&client_id=' + escape(clientId) +
+            '&redirect_uri=' + escape(redirectUri);
         };
-        var startWith = function(string, str) {
-            return (string.substr(0, str.length) === str);
+        var startWith = function (string, str) {
+          return (string.substr(0, str.length) === str);
         };
         var deferred = $q.defer();
-        if(window.cordova) {
-            var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
-            if(cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
-                var browserRef = window.open(getAuthorizeUrl(loginUrl, clientId, redirectUri), "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
-                browserRef.addEventListener("loadstart", function(event) {
-                    if(startWith(event.url, redirectUri)) {
-                        var oauthResponse = {};
+        if (window.cordova) {
+          var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
+          if (cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
+            var browserRef = window.open(getAuthorizeUrl(loginUrl, clientId, redirectUri), "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+            browserRef.addEventListener("loadstart", function (event) {
+              if (startWith(event.url, redirectUri)) {
+                var oauthResponse = {};
 
-                        var fragment = (event.url).split('#')[1];
+                var fragment = (event.url).split('#')[1];
 
-                        if (fragment) {
-                            var nvps = fragment.split('&');
-                            for (var nvp in nvps) {
-                                var parts = nvps[nvp].split('=');
-                                oauthResponse[parts[0]] = unescape(parts[1]);
-                            }
-                        }
-
-                        if (typeof oauthResponse === 'undefined' ||
-                            typeof oauthResponse.access_token === 'undefined') {
-                                deferred.reject("Problem authenticating");
-                            } else {
-                                deferred.resolve(oauthResponse);
-                            }
-                            browserRef.close();
-                        }
-                    });
-                    browserRef.addEventListener('exit', function(event) {
-                        deferred.reject("The sign in flow was canceled");
-                    });
-                } else {
-                    deferred.reject("Could not find InAppBrowser plugin");
+                if (fragment) {
+                  var nvps = fragment.split('&');
+                  for (var nvp in nvps) {
+                    var parts = nvps[nvp].split('=');
+                    oauthResponse[parts[0]] = unescape(parts[1]);
+                  }
                 }
-            } else {
-                deferred.reject("Cannot authenticate via a web browser");
-            }
-            return deferred.promise;
-        },
 
-        /*
-        * Sign into the Strava service
-        *
-        * @param    string clientId
-        * @param    string clientSecret
-        * @param    array appScope
-        * @return   promise
-        */
-        strava: function(clientId, clientSecret, appScope) {
-            var deferred = $q.defer();
-            if(window.cordova) {
-                var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
-                if(cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
-                    var browserRef = window.open('https://www.strava.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(",") + '&response_type=code&approval_prompt=force', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-                    browserRef.addEventListener('loadstart', function(event) {
-                        if((event.url).indexOf("http://localhost/callback") === 0) {
-                            requestToken = (event.url).split("code=")[1];
-                            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-                            $http({method: "post", url: "https://www.strava.com/oauth/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&code=" + requestToken })
-                            .success(function(data) {
-                                deferred.resolve(data);
-                            })
-                            .error(function(data, status) {
-                                deferred.reject("Problem authenticating");
-                            });
-                            browserRef.close();
-                        }
-                    });
-                    browserRef.addEventListener('exit', function(event) {
-                        deferred.reject("The sign in flow was canceled");
-                    });
+                if (typeof oauthResponse === 'undefined' ||
+                  typeof oauthResponse.access_token === 'undefined') {
+                  deferred.reject("Problem authenticating");
                 } else {
-                    deferred.reject("Could not find InAppBrowser plugin");
+                  deferred.resolve(oauthResponse);
                 }
-            } else {
-                deferred.reject("Cannot authenticate via a web browser");
-            }
-            return deferred.promise;
+                browserRef.close();
+              }
+            });
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
+            });
+          } else {
+            deferred.reject("Could not find InAppBrowser plugin");
+          }
+        } else {
+          deferred.reject("Cannot authenticate via a web browser");
         }
+        return deferred.promise;
+      },
+
+      /*
+       * Sign into the Strava service
+       *
+       * @param    string clientId
+       * @param    string clientSecret
+       * @param    array appScope
+       * @return   promise
+       */
+      strava: function (clientId, clientSecret, appScope) {
+        var deferred = $q.defer();
+        if (window.cordova) {
+          var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
+          if (cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
+            var browserRef = window.open('https://www.strava.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(",") + '&response_type=code&approval_prompt=force', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+            browserRef.addEventListener('loadstart', function (event) {
+              if ((event.url).indexOf("http://localhost/callback") === 0) {
+                requestToken = (event.url).split("code=")[1];
+                $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                $http({method: "post", url: "https://www.strava.com/oauth/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&code=" + requestToken})
+                  .success(function (data) {
+                    deferred.resolve(data);
+                  })
+                  .error(function (data, status) {
+                    deferred.reject("Problem authenticating");
+                  })
+                  .finally(function () {
+                    browserRef.close();
+                  });
+              }
+            });
+            browserRef.addEventListener('exit', function (event) {
+              deferred.reject("The sign in flow was canceled");
+            });
+          } else {
+            deferred.reject("Could not find InAppBrowser plugin");
+          }
+        } else {
+          deferred.reject("Cannot authenticate via a web browser");
+        }
+        return deferred.promise;
+      }
 
     };
   }]);
