c3d93f316ad776b0a5cf589235f5839f8147bf81
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2015-01-20 21:51:31 -0500

    feat(demo): add icon + start of file plugin

--- demo/www/app/file/file.ctrl.js
+++ demo/www/app/file/file.ctrl.js
@@ -2,4 +2,16 @@ angular.module('demo.file.ctrl', [])
 
   .controller('FileCtrl', function ($scope, $log, $cordovaFile) {
 
+
+    document.addEventListener('deviceready', function () {
+      $cordovaFile.checkDir("/directory").then(function (success) {
+        console.log('success ' + JSON.stringify(success));
+        $scope.isDir = success;
+      }, function (error) {
+        console.log('success ' + JSON.stringify(error));
+        $scope.isDir = error;
+      })
+    })
+
+
   });
