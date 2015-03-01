b53e994f4a0f18a69679c602f670fa699021b845
Alexandre Oliveira <alexandreoliveira@MacBook-Pro-de-Alexandre.local>
2015-03-01 14:19:18 -0300

    corrections suggested by pbernasconi

--- src/plugins/insomnia.js
+++ src/plugins/insomnia.js
@@ -1,4 +1,5 @@
-
+// install  :     cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
+// link     :     https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin
 angular.module('ngCordova.plugins.insomnia', [])
 
   .factory('$cordovaInsomnia', ['$window', function ($window) {
