1d6bf8bab2d750c119f4fe7d7869c74fcb86bab1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-03-31 17:29:48 +0200

    another build

--- src/plugins/module.js
+++ src/plugins/module.js
@@ -67,5 +67,6 @@ angular.module('ngCordova.plugins', [
   'ngCordova.plugins.vibration',
   'ngCordova.plugins.videoCapturePlus',
   'ngCordova.plugins.zip',
-  'ngCordova.plugins.insomnia'
+  'ngCordova.plugins.insomnia',
+  'ngCordova.plugins.upsPush'
 ]);
