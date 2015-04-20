61645fd9adf2d17857ff284fa6dae348142f2411
paroos <pa.roos@icloud.com>
2015-04-20 17:23:28 +0200

    Enhance the camera mock with the imageData url

--- test/mocks/camera.spec.js
+++ test/mocks/camera.spec.js
@@ -27,6 +27,7 @@ describe('ngCordovaMocks', function() {
 
 		it('should throw an error while getting the picture.', function(done) {
 			$cordovaCamera.throwsError = true;
+      $cordovaCamera.imageData = '';
 			$cordovaCamera.getPicture(cameraOptions)
 				.then(
 					function() { expect(true).toBe(false); },
@@ -37,5 +38,41 @@ describe('ngCordovaMocks', function() {
 
 			$rootScope.$digest();
 		});
-	});
+
+    it('should return the url of the picture.', function(done) {
+      var expected = 'file:///mnt/sdcard/dummy/myImage.jpeg';
+
+      $cordovaCamera.throwsError = false;
+      $cordovaCamera.imageData = expected;
+      $cordovaCamera.getPicture(cameraOptions)
+                    .then(
+                            function(imageData) {
+                              expect(imageData).toEqual(expected);
+                            },
+                            function() {
+                              expected(false).toBe(true);
+                            } )
+                    .finally(
+                            function() {
+                              done(); });
+
+      $rootScope.$digest();
+    });
+
+    it('should return an empty string as imageData.', function(done) {
+      var expected = '';
+      $cordovaCamera.throwsError = false;
+      $cordovaCamera.getPicture(cameraOptions)
+                    .then(
+                            function(imageData) {
+                              expect(imageData).toMatch('');
+                            },
+                            function() {
+                              expect(false).toBe(true);
+                            })
+                    .finally(function() { done(); });
+
+      $rootScope.$digest();
+    });
+  });
 })
