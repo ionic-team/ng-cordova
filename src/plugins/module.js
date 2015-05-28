cde2d494dcaf3a57dbff8c8adad94eb31413de7c
Wladiston Paiva <desenvolvedormg@gmail.com>
2015-05-28 00:03:33 -0300

    Added support for plugin QRCodeScanner.

--- src/plugins/module.js
+++ src/plugins/module.js
@@ -57,6 +57,7 @@ angular.module('ngCordova.plugins', [
   'ngCordova.plugins.printer',
   'ngCordova.plugins.progressIndicator',
   'ngCordova.plugins.push',
+  'ngCordova.plugins.qrcodeScanner',
   'ngCordova.plugins.sms',
   'ngCordova.plugins.socialSharing',
   'ngCordova.plugins.spinnerDialog',
