cbc4b3756495e4e1236f2b6262f7ccce57cda753
Tom Esposito <Tom@Toms-MacBook-Pro.local>
2015-08-02 22:24:09 -0500

    added increase and decrease functions and cleaned up a little

--- src/plugins/badge.js
+++ src/plugins/badge.js
@@ -12,7 +12,7 @@ angular.module('ngCordova.plugins.badge', [])
           if (permission) {
             q.resolve(true);
           } else {
-            q.reject("You do not have permission");
+            q.reject('You do not have permission');
           }
         });
 
@@ -23,14 +23,16 @@ angular.module('ngCordova.plugins.badge', [])
         return cordova.plugins.notification.badge.promptForPermission();
       },
 
-      set: function (number) {
+      set: function (badge, callback, scope) {
         var q = $q.defer();
 
         cordova.plugins.notification.badge.hasPermission(function (permission) {
           if (permission) {
-            q.resolve(cordova.plugins.notification.badge.set(number));
+            q.resolve(
+              cordova.plugins.notification.badge.set(badge, callback, scope)
+            );
           } else {
-            q.reject("You do not have permission to set Badge");
+            q.reject('You do not have permission to set Badge');
           }
         });
         return q.promise;
@@ -44,23 +46,51 @@ angular.module('ngCordova.plugins.badge', [])
               q.resolve(badge);
             });
           } else {
-            q.reject("You do not have permission to get Badge");
+            q.reject('You do not have permission to get Badge');
           }
         });
 
         return q.promise;
       },
 
-      clear: function () {
+      clear: function (callback, scope) {
         var q = $q.defer();
 
         cordova.plugins.notification.badge.hasPermission(function (permission) {
           if (permission) {
-            q.resolve(cordova.plugins.notification.badge.clear());
+            q.resolve(cordova.plugins.notification.badge.clear(callback, scope));
           } else {
-            q.reject("You do not have permission to clear Badge");
+            q.reject('You do not have permission to clear Badge');
           }
         });
+        return q.promise;
+      },
+
+      increase: function (count, callback, scope) {
+        var q = $q.defer();
+
+        this.hasPermission().then(function (){
+          q.resolve(
+            cordova.plugins.notification.badge.increase(count, callback, scope)
+          );
+        }, function (){
+          q.reject('You do not have permission to increase Badge');
+        }) ;
+
+        return q.promise;
+      },
+
+      decrease: function (count, callback, scope) {
+        var q = $q.defer();
+
+        this.hasPermission().then(function (){
+          q.resolve(
+            cordova.plugins.notification.badge.decrease(count, callback, scope)
+          );
+        }, function (){
+          q.reject('You do not have permission to decrease Badge');
+        }) ;
+
         return q.promise;
       },
 
