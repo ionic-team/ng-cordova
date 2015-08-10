a859b98cf512a6b9521399970482e2248ed34912
Max Lynch <max@drifty.com>
2015-08-10 15:04:42 -0500

    Shjit

--- demo2/www/_app/app.bundle.js
+++ demo2/www/_app/app.bundle.js
@@ -70,6 +70,80 @@ System.register("app/app", ["ionic/ionic", "./pages/camera", "./pages/geolocatio
         }
     };
 });
+System.register("app/playlists/playlists", ["angular2/angular2", "ionic/ionic"], function (_export) {
+    "use strict";
+
+    var NgFor, Component, View, IonicApp, NavController, Navbar, NavbarTemplate, List, Item, Content, __decorate, __metadata, PlaylistsPage;
+
+    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+    return {
+        setters: [function (_angular2Angular2) {
+            NgFor = _angular2Angular2.NgFor;
+            Component = _angular2Angular2.Component;
+            View = _angular2Angular2.View;
+        }, function (_ionicIonic) {
+            IonicApp = _ionicIonic.IonicApp;
+            NavController = _ionicIonic.NavController;
+            Navbar = _ionicIonic.Navbar;
+            NavbarTemplate = _ionicIonic.NavbarTemplate;
+            List = _ionicIonic.List;
+            Item = _ionicIonic.Item;
+            Content = _ionicIonic.Content;
+        }],
+        execute: function () {
+            __decorate = undefined && undefined.__decorate || function (decorators, target, key, desc) {
+                if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+                switch (arguments.length) {
+                    case 2:
+                        return decorators.reduceRight(function (o, d) {
+                            return d && d(o) || o;
+                        }, target);
+                    case 3:
+                        return decorators.reduceRight(function (o, d) {
+                            return (d && d(target, key), void 0);
+                        }, void 0);
+                    case 4:
+                        return decorators.reduceRight(function (o, d) {
+                            return d && d(target, key, o) || o;
+                        }, desc);
+                }
+            };
+
+            __metadata = undefined && undefined.__metadata || function (k, v) {
+                if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+            };
+
+            PlaylistsPage = (function () {
+                var _class = function PlaylistsPage(app, nav) {
+                    _classCallCheck(this, _class);
+
+                    this.app = app;
+                    this.playlists = [{ title: "90's Punk" }, { title: "80's Revival" }];
+                };
+
+                _createClass(_class, [{
+                    key: "toggleMenu",
+                    value: function toggleMenu() {
+                        var aside = this.app.getComponent("mainMenu");
+                        aside.toggle();
+                    }
+                }]);
+
+                return _class;
+            })();
+
+            _export("PlaylistsPage", PlaylistsPage);
+
+            _export("PlaylistsPage", PlaylistsPage = __decorate([Component({ selector: "ion-view" }), View({
+                directives: [NgFor, Content, List, Item, Navbar, NavbarTemplate],
+                templateUrl: "_app/playlists/playlists.html"
+            }), __metadata("design:paramtypes", [typeof IonicApp !== "undefined" && IonicApp || Object, typeof NavController !== "undefined" && NavController || Object])], PlaylistsPage));
+        }
+    };
+});
 System.register("app/pages/camera", ["angular2/angular2", "ionic/ionic", "ng-cordova/ng-cordova"], function (_export) {
     "use strict";
 
@@ -267,80 +341,6 @@ System.register('app/pages/index', ['./camera', './geolocation'], function (_exp
     }],
     execute: function () {}
   };
-});
-System.register("app/playlists/playlists", ["angular2/angular2", "ionic/ionic"], function (_export) {
-    "use strict";
-
-    var NgFor, Component, View, IonicApp, NavController, Navbar, NavbarTemplate, List, Item, Content, __decorate, __metadata, PlaylistsPage;
-
-    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-    return {
-        setters: [function (_angular2Angular2) {
-            NgFor = _angular2Angular2.NgFor;
-            Component = _angular2Angular2.Component;
-            View = _angular2Angular2.View;
-        }, function (_ionicIonic) {
-            IonicApp = _ionicIonic.IonicApp;
-            NavController = _ionicIonic.NavController;
-            Navbar = _ionicIonic.Navbar;
-            NavbarTemplate = _ionicIonic.NavbarTemplate;
-            List = _ionicIonic.List;
-            Item = _ionicIonic.Item;
-            Content = _ionicIonic.Content;
-        }],
-        execute: function () {
-            __decorate = undefined && undefined.__decorate || function (decorators, target, key, desc) {
-                if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
-                switch (arguments.length) {
-                    case 2:
-                        return decorators.reduceRight(function (o, d) {
-                            return d && d(o) || o;
-                        }, target);
-                    case 3:
-                        return decorators.reduceRight(function (o, d) {
-                            return (d && d(target, key), void 0);
-                        }, void 0);
-                    case 4:
-                        return decorators.reduceRight(function (o, d) {
-                            return d && d(target, key, o) || o;
-                        }, desc);
-                }
-            };
-
-            __metadata = undefined && undefined.__metadata || function (k, v) {
-                if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-            };
-
-            PlaylistsPage = (function () {
-                var _class = function PlaylistsPage(app, nav) {
-                    _classCallCheck(this, _class);
-
-                    this.app = app;
-                    this.playlists = [{ title: "90's Punk" }, { title: "80's Revival" }];
-                };
-
-                _createClass(_class, [{
-                    key: "toggleMenu",
-                    value: function toggleMenu() {
-                        var aside = this.app.getComponent("mainMenu");
-                        aside.toggle();
-                    }
-                }]);
-
-                return _class;
-            })();
-
-            _export("PlaylistsPage", PlaylistsPage);
-
-            _export("PlaylistsPage", PlaylistsPage = __decorate([Component({ selector: "ion-view" }), View({
-                directives: [NgFor, Content, List, Item, Navbar, NavbarTemplate],
-                templateUrl: "_app/playlists/playlists.html"
-            }), __metadata("design:paramtypes", [typeof IonicApp !== "undefined" && IonicApp || Object, typeof NavController !== "undefined" && NavController || Object])], PlaylistsPage));
-        }
-    };
 });
 System.register("app/songs/song-detail", ["angular2/angular2", "ionic/ionic"], function (_export) {
     "use strict";
