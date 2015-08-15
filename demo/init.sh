d44d3325a92e38c48808069ea7105cc85fae9d03
Danail <DanailMinchev@users.noreply.github.com>
2015-08-15 13:31:58 +0100

    Added $cordovaBeacon plugin

--- demo/init.sh
+++ demo/init.sh
@@ -39,3 +39,4 @@ cordova plugin add https://github.com/EddyVerbruggen/cordova-plugin-actionsheet.
 cordova plugin add https://github.com/Telerik-Verified-Plugins/HealthKit
 cordova plugin add de.appplant.cordova.plugin.badge
 cordova plugin add aerogear-cordova-push
+cordova plugin add com.unarin.cordova.beacon
