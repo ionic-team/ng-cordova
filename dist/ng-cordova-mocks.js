91358cf7f311817a30107d0a9ad4ee480f87ebfa
Cihad Horuzoglu <cihad.horuzoglu@ciceksepeti.com>
2015-09-30 10:01:06 +0300

    Google Plus Mocks Added
    Google Plus mock methods added to mock directory

--- dist/ng-cordova-mocks.js
+++ dist/ng-cordova-mocks.js
@@ -14,6 +14,11 @@ ngCordovaMocks.factory('$cordovaAppVersion', ['$q', function ($q) {
       var defer = $q.defer();
       defer.resolve('mock v');
       return defer.promise;
+    },
+    getVersionNumber: function() {
+      var defer = $q.defer();
+      defer.resolve('0.0.0');
+      return defer.promise;
     }
   };
 }]);
@@ -2197,6 +2202,68 @@ ngCordovaMocks.factory('$cordovaGooglePlayGame', ['$q', function ($q) {
   };
 
 }]);
+
+/**
+   * @ngdoc service
+   * @name ngCordovaMocks.cordovaGooglePlus
+   *
+   * @description
+   * A service for testing Google Plus features
+   * in an app built with ngCordova.
+   **/
+  ngCordovaMocks.factory('$cordovaGooglePlus', ['$q', function ($q) {
+    return {
+
+      /**
+       * These properties are here for the purpose of automated testing only.
+       **/
+      loginShouldSucceedWith: null,
+      silentLoginShouldSucceedWith: null,
+      disconnectShouldSucceedWith: null,
+      isAvailableShouldSucceedWith: null,
+      logoutShouldSuceedWith: null,
+
+      login: function (options) {
+        if (this.loginShouldSucceedWith !== null) {
+          return $q.when(this.loginShouldSucceedWith);
+        } else {
+          return $q.reject();
+        }
+      },
+
+      silentLogin: function (options) {
+        if (this.loginShouldSucceedWith !== null) {
+          return $q.when(this.silentLoginShouldSucceedWith);
+        } else {
+          return $q.reject();
+        }
+      },
+
+      logout: function () {
+        if (this.logoutShouldSuceedWith !== null) {
+          return $q.when(this.logoutShouldSuceedWith);
+        } else {
+          return $q.reject();
+        }
+      },
+
+      disconnect: function () {
+        if (this.logoutShouldSuceedWith !== null) {
+          return $q.when(this.disconnectShouldSucceedWith);
+        } else {
+          return $q.reject();
+        }
+      },
+
+      isAvailable: function () {
+        if (this.logoutShouldSuceedWith !== null) {
+          return $q.when(this.disconnectShouldSucceedWith);
+        } else {
+          return $q.reject();
+        }
+      }
+    };
+  }]);
 
 /**
  * @ngdoc service
