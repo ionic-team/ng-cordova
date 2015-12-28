07c778e48a3ff5bcdfb79fbe507ec1b848bb6cea
njleonzhang <zpczjl@126.com>
2015-12-28 13:52:44 +0800

    [android] Handle 'cancel'
    On android, if the dat picker is closed by button ‘cancel’, onError is
    triggered according to the plugin doc. In this situation, we should
    reject the promise.
    
    ============
    datePicker.show(options, onSuccess, onError);
    ============

--- src/plugins/datepicker.js
+++ src/plugins/datepicker.js
@@ -11,6 +11,8 @@ angular.module('ngCordova.plugins.datePicker', [])
         options = options || {date: new Date(), mode: 'date'};
         $window.datePicker.show(options, function (date) {
           q.resolve(date);
+        }, function(error){
+          q.reject(error);
         });
         return q.promise;
       }
