ce8dedd13b0c3f7d029bb9da9c148c7b9f5f83e5
Ibrahim Hadeed <ibby93@gmail.com>
2016-01-15 23:09:14 -0500

    fixed depreciated vibrate plugin functions

--- src/plugins/vibration.js
+++ src/plugins/vibration.js
@@ -6,14 +6,8 @@ angular.module('ngCordova.plugins.vibration', [])
   .factory('$cordovaVibration', [function () {
 
     return {
-      vibrate: function (times) {
-        return navigator.notification.vibrate(times);
-      },
-      vibrateWithPattern: function (pattern, repeat) {
-        return navigator.notification.vibrateWithPattern(pattern, repeat);
-      },
-      cancelVibration: function () {
-        return navigator.notification.cancelVibration();
+      vibrate: function (timeOrPattern) {
+        return navigator.vibrate(timeOrPattern);
       }
     };
   }]);
