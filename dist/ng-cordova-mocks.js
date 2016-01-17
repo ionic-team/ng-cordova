2b01b041fb682610d5ddfbc7755f6c361e2ac87d
Sachin Bansal <sachin@sifium.co>
2016-01-17 21:01:41 +0530

    Google Plus wrapper plugin

--- dist/ng-cordova-mocks.js
+++ dist/ng-cordova-mocks.js
@@ -2025,6 +2025,69 @@ ngCordovaMocks.factory('$cordovaGlobalization', ['$q', function ($q) {
 
 /**
  * @ngdoc service
+ * @name ngCordovaMocks.cordovaGoogle
+ *
+ * @description
+ * A service for testing Google features
+ * in an app built with ngCordova.
+ **/
+ngCordovaMocks.factory('$cordovaGoogle', ['$q', function ($q) {
+  return {
+
+    /**
+     * These properties are here for the purpose of automated testing only.
+     **/
+    isAvailableShouldSucceedWith: null,
+    loginShouldSucceedWith: null,
+    trySilentLoginShouldSucceedWith: null,
+    logoutShouldSuceedWith: null,
+    disconnectShouldSuceedWith: null,
+
+    isAvailable: function () {
+      if (this.loginShouldSucceedWith !== null) {
+        return $q.when(this.loginShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    login: function () {
+      if (this.loginShouldSucceedWith !== null) {
+        return $q.when(this.loginShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    trySilentLogin: function () {
+      if (this.trySilentLoginShouldSucceedWith !== null) {
+        return $q.when(this.trySilentLoginShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    logout: function () {
+      if (this.logoutShouldSuceedWith !== null) {
+        return $q.when(this.logoutShouldSuceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    disconnect: function () {
+      if (this.disconnectShouldSuceedWith !== null) {
+        return $q.when(this.disconnectShouldSuceedWith);
+      } else {
+        return $q.reject();
+      }
+    }
+  };
+}]);
+
+
+/**
+ * @ngdoc service
  * @name ngCordovaMocks.cordovaGoogleAnalytics
  *
  * @description
--- dist/ng-cordova.js
+++ dist/ng-cordova.js
@@ -3533,6 +3533,70 @@ angular.module('ngCordova.plugins.globalization', [])
     };
   }]);
 
+// install   :   cordova plugin add cordova-plugin-googleplus --variable REVERSED_CLIENT_ID=myreversedclientid
+// link      :   https://github.com/EddyVerbruggen/cordova-plugin-googleplus
+
+/* globals $window.plugins.googleplus: true */
+angular.module('ngCordova.plugins.google', [])
+
+  .provider('$cordovaGoogle', [function () {
+
+    this.$get = ['$q', '$window', function ($q, $window) {
+      return {
+        isAvailable: function () {
+          var q = $q.defer();
+          $window.plugins.googleplus.isAvailable(function(available) {
+            if (available) {
+              q.resolve(available);
+            } else {
+              q.reject(available);
+            }
+          });
+          return q.promise;
+        },
+
+        login: function (data) {
+          var q = $q.defer();
+          $window.plugins.googleplus.login(data, function (obj) {
+            q.resolve(obj);
+          }, function (msg) {
+            q.reject(msg);
+          });
+
+          return q.promise;
+        },
+
+        trySilentLogin: function (data) {
+          var q = $q.defer();
+          $window.plugins.googleplus.trySilentLogin(data, function (obj) {
+            q.resolve(obj);
+          }, function (msg) {
+            q.reject(msg);
+          });
+
+          return q.promise;
+        },
+
+        logout: function () {
+          var q = $q.defer();
+          $window.plugins.googleplus.logout(function (res) {
+            q.resolve(res);
+          });
+          return q.promise;
+        },
+
+        disconnect: function () {
+          var q = $q.defer();
+          $window.plugins.googleplus.disconnect(function (res) {
+            q.resolve(res);
+          });
+          return q.promise;
+        }
+      };
+    }];
+  }]);
+
+
 // install  :     cordova plugin add https://github.com/floatinghotpot/cordova-admob-pro.git
 // link     :     https://github.com/floatinghotpot/cordova-admob-pro
 
