4329c43d469b8c83f1f352a6c7c2efbcaca02a12
allenconquest <allen.conquest@valexa.com>
2016-04-15 10:32:23 +0100

    added additional test for options in spinnerDialog.show() method

--- test/plugins/spinnerDialog.spec.js
+++ test/plugins/spinnerDialog.spec.js
@@ -12,9 +12,14 @@ describe('Service: $cordovaSpinnerDialog', function() {
     }
   }));
 
-  it('should call window.plugins\'s spinnerDialog.show', function() {
+  it('should call window.plugins\'s spinnerDialog.show method with options', function() {
+    $cordovaSpinnerDialog.show('hi', 'nice hi message', true, {overlayOpacity: 0.75,  textColorRed: 1, textColorGreen: 0, textColorBlue: 0.4});
+    expect(window.plugins.spinnerDialog.show).toHaveBeenCalledWith('hi', 'nice hi message', true, {overlayOpacity: 0.75,  textColorRed: 1, textColorGreen: 0, textColorBlue: 0.4});
+  });
+
+  it('should call window.plugins\'s spinnerDialog.show method without options', function() {
     $cordovaSpinnerDialog.show('hi', 'nice hi message', true);
-    expect(window.plugins.spinnerDialog.show).toHaveBeenCalledWith('hi', 'nice hi message', true);
+    expect(window.plugins.spinnerDialog.show).toHaveBeenCalledWith('hi', 'nice hi message', true, undefined);
   });
 
   it('should call window.plugins\'s spinnerDialog.hide', function() {
