e31b4f3b5481cf354a09690e93ae347828c3e17c
Jacques Bouman <jacques.bouman@gmail.com>
2016-04-21 20:21:20 +0200

    Updated appVersion mocks and added tests

--- src/mocks/appVersion.js
+++ src/mocks/appVersion.js
@@ -2,10 +2,29 @@ ngCordovaMocks.factory('$cordovaAppVersion', ['$q', function ($q) {
   var throwsError = false;
   return {
     throwsError: throwsError,
-    getAppVersion: function () {
-      var defer = $q.defer();
-      defer.resolve('mock v');
-      return defer.promise;
+
+    getAppName: function () {
+      var q = $q.defer();
+      q.resolve('mock app name');
+      return q.promise;
+    },
+
+    getPackageName: function () {
+      var q = $q.defer();
+      q.resolve('com.package.mock');
+      return q.promise;
+    },
+
+    getVersionNumber: function () {
+      var q = $q.defer();
+      q.resolve('1.2.3');
+      return q.promise;
+    },
+
+    getVersionCode: function () {
+      var q = $q.defer();
+      q.resolve('4.5.6');
+      return q.promise;
     }
   };
 }]);
