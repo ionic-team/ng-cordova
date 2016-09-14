772536baf533468c5dd10bfa3245b0ae23219062
Niklas Merz <NiklasMerz@gmx.net>
2016-09-14 17:25:38 +0200

    Add fingerprint aio plugin

---
+++ src/plugins/fingerprint.js
@@ -0,0 +1,41 @@
+// install   :      cordova plugin add https://github.com/NiklasMerz/cordova-plugin-fingerprint-aio
+// link      :      https://github.com/NiklasMerz/cordova-plugin-fingerprint-aio
+
+/* globals fingerprint: true */
+angular.module('ngCordova.plugins.fingerprint', [])
+
+  .factory('$cordovaFingerprint', ['$q', function ($q) {
+
+    return {
+      isAvailable: function () {
+        var defer = $q.defer();
+        if (!window.cordova) {
+          defer.reject('Not supported without cordova.js');
+        } else {
+          fingerprint.isAvailable(function (value) {
+            defer.resolve(value);
+          }, function (err) {
+            defer.reject(err);
+          });
+        }
+
+        return defer.promise;
+      },
+
+      show: function (options) {
+        var defer = $q.defer();
+        if (!window.cordova) {
+          defer.reject('Not supported without cordova.js');
+        } else {
+          fingerprint.authenticate(options,
+          function (value) {
+            defer.resolve(value);
+          }, function (err) {
+            defer.reject(err);
+          });
+        }
+
+        return defer.promise;
+      }
+    };
+  }]);
