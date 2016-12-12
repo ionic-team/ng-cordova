926d17ac4fd1a6d4260804c77b74c755df48a935
cxb6046 <CHRISTIAN_X_BOTKIN@homedepot.com>
2016-12-12 15:38:43 -0500

    Google Analytics: API Update
    The Google Analytics package that this project calls has been updated in a backwards incompatible manner.  These edits are updates to address these changes.

--- src/plugins/googleAnalytics.js
+++ src/plugins/googleAnalytics.js
@@ -6,10 +6,10 @@ angular.module('ngCordova.plugins.googleAnalytics', [])
   .factory('$cordovaGoogleAnalytics', ['$q', '$window', function ($q, $window) {
 
     return {
-      startTrackerWithId: function (id) {
+      startTrackerWithId: function (id, dispatchPeriod) {
         var d = $q.defer();
 
-        $window.analytics.startTrackerWithId(id, function (response) {
+        $window.analytics.startTrackerWithId(id, dispatchPeriod, function (response) {
           d.resolve(response);
         }, function (error) {
           d.reject(error);
@@ -42,10 +42,10 @@ angular.module('ngCordova.plugins.googleAnalytics', [])
         return d.promise;
       },
 
-      trackView: function (screenName) {
+      trackView: function (screenName, campaingUrl, newSession) {
         var d = $q.defer();
 
-        $window.analytics.trackView(screenName, function (response) {
+        $window.analytics.trackView(screenName, campaingUrl, newSession, function (response) {
           d.resolve(response);
         }, function (error) {
           d.reject(error);
@@ -71,10 +71,10 @@ angular.module('ngCordova.plugins.googleAnalytics', [])
         return d.promise;
       },
 
-      trackEvent: function (category, action, label, value) {
+      trackEvent: function (category, action, label, value, newSession) {
         var d = $q.defer();
 
-        $window.analytics.trackEvent(category, action, label, value, function (response) {
+        $window.analytics.trackEvent(category, action, label, value, newSession, function (response) {
           d.resolve(response);
         }, function (error) {
           d.reject(error);
